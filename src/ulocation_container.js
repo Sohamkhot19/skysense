aecce61f34c4e05d3cdb232b966b6b7d603846b2
Sohamkhot19 <sohamkhot2004@gmail.com>
2024-02-16 21:52:46 +0530

    Initial commit

---
+++ src/ulocation_container.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./ulocation.css";
+export class ulocation_container extends Component {
+  render() {
+    return (
+      <>
+      <div className="section">
+        <div className="container1">
+            <h1>Mumbai</h1>
+            <h4>India</h4>
+        </div>
+        <div className="container2">
+            <h1>Mumbai</h1>
+            <h4>India</h4>
+        </div>
+      </div>
+        
+      </>
+    );
+  }
+}
+
+export default ulocation_container;
