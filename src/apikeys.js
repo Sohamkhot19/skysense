2ebf1f39524b46ea33a799d9596342702c1ae056
Sohamkhot19 <sohamkhot2004@gmail.com>
2024-03-02 17:42:21 +0530

    api integrated
    api integrated in app
    modified state in app

---
+++ src/apikeys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    key: "d881078b11886de10ed70a251803bbb2",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
--- src/ulocation_container.js
+++ src/ulocation_container.js
@@ -1,30 +1,97 @@
 import React, { Component } from "react";
 import "./ulocation.css";
-import Forecast_container from './forecast_container';
+import Forecast_container from "./forecast_container";
+import apiKeys from "./apikeys";
+import loader from "./WeatherIcons.gif";
+import ReactAnimatedWeather from "react-animated-weather";
+const defaults = {
+  color: "white",
+  size: 112,
+  animate: true,
+};
 export class ulocation_container extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentTime: '',
-      greeting:''
+      currentTime: "",
+      greeting: "",
+      photo: null,
+      color: null,
+      lat: undefined,
+      lon: undefined,
+      errorMessage: undefined,
+      temperatureC: undefined,
+      temperatureF: undefined,
+      city: undefined,
+      country: undefined,
+      humidity: undefined,
+      description: undefined,
+      icon: "CLEAR_DAY",
+      sunrise: undefined,
+      sunset: undefined,
+      errorMsg: undefined,
     };
   }
   determineGreeting = () => {
     const currentHour = new Date().getHours();
-    let greeting = '';
+    let greeting = "";
     if (currentHour >= 5 && currentHour < 12) {
-      greeting = 'Good Morning';
+      greeting = "Good Morning";
     } else if (currentHour >= 12 && currentHour < 17) {
-      greeting = 'Good Afternoon';
+      greeting = "Good Afternoon";
     } else if (currentHour >= 17 && currentHour < 22) {
-      greeting = 'Good Evening';
+      greeting = "Good Evening";
     } else {
-      greeting = 'Good Night';
+      greeting = "Good Night";
     }
 
-    this.setState({greeting});
+    this.setState({ greeting });
   };
-
+  determinephoto = () => {
+    const currentHour = new Date().getHours();
+    let photo = null;
+    if (currentHour >= 5 && currentHour < 9) {
+      import("./early-morning.jpg").then((image) => {
+        this.setState({ photo: image.default, color: "white" });
+      });
+    }
+    if (currentHour >= 9 && currentHour < 12) {
+      import("./late-morning.jpg").then((image) => {
+        this.setState({ photo: image.default, color: "black" });
+      });
+    }
+    if (currentHour >= 12 && currentHour < 15) {
+      import("./afternoon.jpg").then((image) => {
+        this.setState({ photo: image.default, color: "black" });
+      });
+    }
+    if (currentHour >= 15 && currentHour < 17) {
+      import("./late-afternoon.jpg").then((image) => {
+        this.setState({ photo: image.default, color: "black" });
+      });
+    }
+    if (currentHour >= 17 && currentHour < 19) {
+      import("./evening.jpg").then((image) => {
+        this.setState({ photo: image.default, color: "white" });
+      });
+    }
+    if (currentHour >= 19 && currentHour < 20) {
+      import("./late-evening.jpg").then((image) => {
+        this.setState({ photo: image.default, color: "white" });
+      });
+    }
+    if (currentHour >= 20 && currentHour < 0) {
+      import("./early-night.jpg").then((image) => {
+        this.setState({ photo: image.default, color: "white" });
+      });
+    }
+    if (currentHour >= 0 && currentHour < 5) {
+      import("./night.jpg").then((image) => {
+        this.setState({ photo: image.default, color: "white" });
+      });
+    }
+    this.setState({ photo });
+  };
   startTime = () => {
     const today = new Date();
     let h = today.getHours();
@@ -35,48 +102,172 @@ export class ulocation_container extends Component {
   };
 
   componentDidMount() {
+    if (navigator.geolocation) {
+      this.getPosition()
+        //If user allow location service then will fetch data & send it to get-weather function.
+        .then((position) => {
+          this.getWeather(position.coords.latitude, position.coords.longitude);
+        })
+        .catch((err) => {
+          //If user denied location service then standard location weather will le shown on basis of latitude & latitude.
+          this.getWeather(28.67, 77.22);
+          alert(
+            "You have disabled location service. Allow 'This APP' to access your location. Your current location will be used for calculating Real time weather."
+          );
+        });
+    } else {
+      alert("Geolocation not available");
+    }
     this.intervalId = setInterval(() => {
       this.startTime();
       this.determineGreeting(); // Call determineGreeting here
+      this.determinephoto();
+      this.getWeather(this.state.lat, this.state.lon);
     }, 1000);
   }
-  
 
   componentWillUnmount() {
     clearInterval(this.intervalId);
   }
+
+  getPosition = (options) => {
+    return new Promise(function (resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+  };
+  getWeather = async (lat, lon) => {
+    try {
+      const api_call = await fetch(
+        `${apiKeys.base}weather?lat=${lat}&lon=${lon}&units=metric&APPID=${apiKeys.key}`
+      );
+
+      const data = await api_call.json();
+
+      // Check if the required properties are available in the response
+      if (data && data.main && data.main.temp) {
+        this.setState({
+          lat: lat,
+          lon: lon,
+          city: data.name,
+          temperatureC: Math.round(data.main.temp),
+          temperatureF: Math.round(data.main.temp * 1.8 + 32),
+          humidity: data.main.humidity,
+          main: data.weather[0].main,
+          country: data.sys.country,
+        });
+
+        // Switch statement and icon setting remains the same
+        switch (this.state.main) {
+          case "Haze":
+            this.setState({ icon: "CLEAR_DAY" });
+            break;
+          case "Clouds":
+            this.setState({ icon: "CLOUDY" });
+            break;
+          case "Rain":
+            this.setState({ icon: "RAIN" });
+            break;
+          case "Snow":
+            this.setState({ icon: "SNOW" });
+            break;
+          case "Dust":
+            this.setState({ icon: "WIND" });
+            break;
+          case "Drizzle":
+            this.setState({ icon: "SLEET" });
+            break;
+          case "Fog":
+            this.setState({ icon: "FOG" });
+            break;
+          case "Smoke":
+            this.setState({ icon: "FOG" });
+            break;
+          case "Tornado":
+            this.setState({ icon: "WIND" });
+            break;
+          default:
+            this.setState({ icon: "CLEAR_DAY" });
+        }
+      } else {
+        console.error("Invalid data structure in the API response");
+      }
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  };
+
   render() {
-    return (
-      <>
-        <div className="section">
-          <div className="head">
-            <p>SkySense | {this.state.greeting}</p>
-          </div>
-          <div className="container1">
-            <h1 id="city">Mumbai</h1>
-            <h4 id="country">India</h4>
-            
-          <div className="weather-info">
-          <svg xmlns="http://www.w3.org/2000/svg" width="100px" height="100px" fill="currentColor" class="bi bi-cloud-sun" viewBox="0 0 16 16">
-            <path d="M7 8a3.5 3.5 0 0 1 3.5 3.555.5.5 0 0 0 .624.492A1.503 1.503 0 0 1 13 13.5a1.5 1.5 0 0 1-1.5 1.5H3a2 2 0 1 1 .1-3.998.5.5 0 0 0 .51-.375A3.5 3.5 0 0 1 7 8m4.473 3a4.5 4.5 0 0 0-8.72-.99A3 3 0 0 0 3 16h8.5a2.5 2.5 0 0 0 0-5z"/>
-            <path d="M10.5 1.5a.5.5 0 0 0-1 0v1a.5.5 0 0 0 1 0zm3.743 1.964a.5.5 0 1 0-.707-.707l-.708.707a.5.5 0 0 0 .708.708zm-7.779-.707a.5.5 0 0 0-.707.707l.707.708a.5.5 0 1 0 .708-.708zm1.734 3.374a2 2 0 1 1 3.296 2.198q.3.423.516.898a3 3 0 1 0-4.84-3.225q.529.017 1.028.129m4.484 4.074c.6.215 1.125.59 1.522 1.072a.5.5 0 0 0 .039-.742l-.707-.707a.5.5 0 0 0-.854.377M14.5 6.5a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1z"/>
-          </svg>
-          <h4 className="weather-type">Clear</h4>
-            <p id="degree">32°c</p>
-          </div>
-            
-            <div className="datetime-content">
-              <div className="datetime">
-                <p id="time">{this.state.currentTime}</p>
-                <p id="date">{new Date().toLocaleDateString('en-US', { weekday: 'long', day: 'numeric', month: 'short', year: 'numeric' })}</p>
+    if (this.state.temperatureC) {
+      return (
+        <>
+          <React.Fragment>
+            <div className="section">
+              <div className="head">
+                <p>SkySense | {this.state.greeting}</p>
               </div>
-              
+              <div
+                className="container1"
+                style={{
+                  backgroundImage: `url(${this.state.photo})`,
+                  color: `${this.state.color}`,
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat",
+                }}
+              >
+                <h1 id="city" style={{ color: `${this.state.color}` }}>
+                  {this.state.city}
+                </h1>
+                <h4 id="country" style={{ color: `${this.state.color}` }}>
+                  {this.state.country}
+                </h4>
+
+                <div className="weather-info">
+                  <ReactAnimatedWeather
+                    icon={this.state.icon}
+                    color={this.state.color}
+                    size={defaults.size}
+                    animate={defaults.animate}
+                  />
+                  <h4 className="weather-type">{this.state.main}</h4>
+                  <p id="degree">{this.state.temperatureC}°c</p>
+                </div>
+
+                <div className="datetime-content">
+                  <div className="datetime">
+                    <p id="time" style={{ color: `${this.state.color}` }}>
+                      {this.state.currentTime}
+                    </p>
+                    <p id="date" style={{ color: `${this.state.color}` }}>
+                      {new Date().toLocaleDateString("en-US", {
+                        weekday: "long",
+                        day: "numeric",
+                        month: "short",
+                        year: "numeric",
+                      })}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <Forecast_container />
             </div>
-          </div>
-          <Forecast_container/>
-        </div>
-      </>
-    );
+          </React.Fragment>
+        </>
+      );
+    } else {
+      return (
+        <React.Fragment>
+          <img src={loader} style={{ width: "50%", WebkitUserDrag: "none" }} />
+          <h3 style={{ color: "white", fontSize: "22px", fontWeight: "600" }}>
+            Detecting your location
+          </h3>
+          <h3 style={{ color: "white", marginTop: "10px" }}>
+            Your current location wil be displayed on the App <br></br> & used
+            for calculating Real time weather.
+          </h3>
+        </React.Fragment>
+      );
+    }
   }
 }
 
